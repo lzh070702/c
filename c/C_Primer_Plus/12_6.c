568ac7b64f949793b36f2b549a930dcf5914efc0
lzh070702 <3346099791@qq.com>
2025-11-07 23:24:43 +0800

    new

---
+++ c/C_Primer_Plus/12_6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void number(int arr[],int seed){
+    srand(seed);
+    for (int i = 0; i < 100; i++){
+        arr[i] = rand() % 10 + 1;
+    }
+}
+
+void statistics(int arr[]){
+    int counts[11] = {0};
+    for (int i = 0; i < 100; i++) {
+        counts[arr[i]-1]++;
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("数字%d出现了%d次\n", i + 1, counts[i]);
+    }
+}
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        unsigned int seed;
+        printf("请输入种子值：\n");
+        scanf("%u", &seed);
+        int arr[100] = {0};
+        number(arr, seed);
+        printf("当种子值为%u时，\n各种数出现的次数：\n",seed);
+        statistics(arr);
+    }
+    return 0;
+}
